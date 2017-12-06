b96d41aaa1a28d0121562381ee438913ac62a5e8
Doan Tuan <tuanngodoan.com>
2017-12-06 21:02:19 +0700

    add validate and check Card.

--- DAL/CardDAL.cs
+++ DAL/CardDAL.cs
@@ -3,11 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DAO;
+using System.Data.SqlClient;
+using System.Data;
 
 namespace DAL
 {
     public class CardDAL
     {
-        
+        public SqlDataReader getCardInfo(string cardNo) 
+        {
+            String cmdString = "SELECT FROM Card WHERE CardNo = @cardNo";
+            SqlCommand cmd =  new SqlCommand();
+            cmd.Parameters.AddWithValue("cardNo",cardNo);
+            SqlDataReader dr = ServiceManager.queryGetData(cmdString,cmd);
+            return dr;
+        }
     }
 }
