42e5b4a563fe8a33ace56dcc671e2d64738a02c8
Doan Tuan <tuanngodoan.com>
2017-12-08 15:17:28 +0700

    add LOGBLL

---
+++ BLL/LogBLL.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL;
+using System.Data;
+
+namespace BLL
+{
+    public class LogBLL
+    {
+        LogDAL logDAL = new LogDAL();
+        public DataTable getLog(string cardNo)
+        {
+            return logDAL.getLogs(cardNo);
+        }
+    }
+}
